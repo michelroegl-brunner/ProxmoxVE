b0e2978b3a0fb99a8bcf686ed09d1f9707dae20f
Michel Roegl-Brunner <michel.roegl-brunner@htl-braunau.at>
2024-11-27 16:29:19 +0100

    Worked in comments from pull request

--- install/snipeit-install.sh
+++ install/snipeit-install.sh
@@ -14,89 +14,91 @@ network_check
 update_os
 
 msg_info "Installing Dependencies"
-$STD apt-get install -y curl
-$STD apt-get install -y git
-$STD apt-get install -y sudo
-$STD apt-get install -y mc
-$STD apt-get install -y nginx
-$STD apt-get install -y php8.2-{bcmath,common,ctype,curl,fileinfo,fpm,gd,iconv,intl,mbstring,mysql,soap,xml,xsl,zip,cli}
-$STD apt-get install -y mariadb-server
+$STD apt-get install -y \
+  curl \
+  composer \
+  git \
+  sudo \
+  mc \
+  nginx \
+  php8.2-{bcmath,common,ctype,curl,fileinfo,fpm,gd,iconv,intl,mbstring,mysql,soap,xml,xsl,zip,cli} \
+  mariadb-server 
 msg_ok "Installed Dependencies"
 
-msg_info "Installing Composer"
-curl -sS https://getcomposer.org/installer -o composer-setup.php
-php composer-setup.php --install-dir=/usr/local/bin --filename=composer
-msg_ok "Installed Composer"
+msg_info "Setting up database"
+DB_NAME=snipeit_db
+DB_USER=snipeit
+DB_PASS=$(openssl rand -base64 18 | tr -dc 'a-zA-Z0-9' | head -c13)
+mysql -u root -e "CREATE DATABASE $DB_NAME;"
+mysql -u root -e "CREATE USER '$DB_USER'@'localhost' IDENTIFIED WITH mysql_native_password AS PASSWORD('$DB_PASS');"
+mysql -u root -e "GRANT ALL ON $DB_NAME.* TO '$DB_USER'@'localhost'; FLUSH PRIVILEGES;"
+{
+    echo "SnipeIT-Credentials"
+    echo "SnipeIT Database User: $DB_USER"
+    echo "SnipeIT Database Password: $DB_PASS"
+    echo "SnipeIT Database Name: $DB_NAME"
+} >> ~/snipeit.creds
+msg_ok "Set up database"
 
-msg_ok "Configre Database\n"
-read -r -p "Enter password for Database user: " password
-echo -e 'CREATE DATABASE snipeit;' | mysql
-echo -e "GRANT ALL ON snipeit.* TO snipeit@localhost identified by '$password';" | mysql
-echo -e 'FLUSH PRIVILEGES;' | mysql
-msg_ok "Configured Database"
+msg_info "Installing Snipe-IT"
+cd /opt
+RELEASE=$(curl -s https://api.github.com/repos/snipe/snipe-it/releases/latest | grep "tag_name" | awk '{print substr($2, 3, length($2)-4) }')
+wget -q "https://github.com/snipe/snipe-it/archive/refs/tags/v${RELEASE}.zip"
+unzip -q v${RELEASE}.zip
+mv snipe-it-${RELEASE} /opt/snipe-it
 
-msg_info "Clone SnipeIT from Github"
-cd /var/www/html
-git clone https://github.com/snipe/snipe-it snipe-it
-msg_ok "Finished cloning"
-
-msg_info "Configure SnipeIT"
-cd snipe-it
 cp .env.example .env
 IPADDRESS=$(hostname -I | awk '{print $1}')
-sed -i "s|^APP_URL=.*|APP_URL=http://$IPADDRESS|" .env
 
-sed -i "s|^DB_DATABASE=.*|DB_DATABASE=snipeit|" .env
-sed -i "s|^DB_USERNAME=.*|DB_USERNAME=snipeit|" .env
-sed -i "s|^DB_PASSWORD=.*|DB_PASSWORD=$password|" .env
-chown -R www-data: /var/www/html/snipe-it
-chmod -R 755 /var/www/html/snipe-it
-git config --global --add safe.directory /var/www/html/snipe-it
+sed -i -e "s|^APP_URL=.*|APP_URL=http://$IPADDRESS|" \
+       -e "s|^DB_DATABASE=.*|DB_DATABASE=$DB_NAME|" \
+       -e "s|^DB_USERNAME=.*|DB_USERNAME=$DB_USER|" \
+       -e "s|^DB_PASSWORD=.*|DB_PASSWORD=$DB_PASS|" .env
 
-msg_ok "Configred SnipeIT"
+chown -R www-data: /opt/snipe-it
+chmod -R 755 /opt/snipe-it
 
-msg_info "Update SnipeIT dependencies"
+
 export COMPOSER_ALLOW_SUPERUSER=1
-composer update --no-plugins --no-scripts
-composer install --no-dev --prefer-source --no-plugins --no-scripts
-msg_ok "Update OK"
+$STD composer update --no-plugins --no-scripts
+$STD composer install --no-dev --prefer-source --no-plugins --no-scripts
 
-msg_info "Generate APP_KEY"
-php artisan key:generate --force
-msg_ok "Done"
+$STD php artisan key:generate --force
+msg_ok "Installed SnipeIT"
 
-msg_info "Configure NGINX"
-echo -e '
+msg_info "Creating Service"
+
+cat <<EOF >/etc/nginx/conf.d/snipeit.conf
 server {
         listen 80;
-        server_name '$IPADDRESS';
-        root /var/www/html/snipe-it/public;
+        root /opt/snipe-it/public;
         
         index index.php;
                 
         location / {
-                try_files $uri $uri/ /index.php?$query_string;
-
+                try_files \$uri \$uri/ /index.php?\$query_string;
         }
         
-        location ~ \.php$ {
-		include fastcgi.conf;
+        location ~ \.php\$ {
+                include fastcgi.conf;
                 include snippets/fastcgi-php.conf;
                 fastcgi_pass unix:/run/php/php8.2-fpm.sock;
-		fastcgi_split_path_info ^(.+\.php)(/.+)$;
-        	fastcgi_param SCRIPT_FILENAME $document_root$fastcgi_script_name;
-        	include fastcgi_params;
+                fastcgi_split_path_info ^(.+\.php)(/.+)\$;
+                fastcgi_param SCRIPT_FILENAME \$document_root\$fastcgi_script_name;
+                include fastcgi_params;
         }
-}' > /etc/nginx/conf.d/snipeit.conf
-systemctl restart nginx
-msg_ok "Configured NGINX"
+}
+EOF
 
-msg_ok "SnipeIT is up and running, head to $IPADDRESS to reach the site"
+systemctl reload nginx
+msg_ok "Configured Service"
+
 
 motd_ssh
 customize
 
 msg_info "Cleaning up"
+rm -rf /opt/v${RELEASE}.zip
 $STD apt-get -y autoremove
 $STD apt-get -y autoclean
 msg_ok "Cleaned"
