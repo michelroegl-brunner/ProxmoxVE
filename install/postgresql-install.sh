d81a1efa5f3ae2693a6d4d5f305b31c9018592fb
Michel Roegl-Brunner <michel.roegl-brunner@htl-braunau.at>
2024-12-02 16:31:55 +0100

    typo

--- install/postgresql-install.sh
+++ install/postgresql-install.sh
@@ -14,10 +14,11 @@ network_check
 update_os
 
 msg_info "Installing Dependencies"
-$STD apt-get install -y curl /
-sudo /
-mc /
-gnupg
+$STD apt-get install -y curl
+$STD apt-get install -y sudo
+$STD apt-get install -y mc
+$STD apt-get install -y gnupg
+    
 msg_ok "Installed Dependencies"
 
 msg_info "Setting up PostgreSQL Repository"
