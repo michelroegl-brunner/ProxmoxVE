e198352b9d2f6d8d360adfb7b8847a7dc813ac99
Michel Roegl-Brunner <michel.roegl-brunner@htl-braunau.at>
2024-12-04 16:08:00 +0100

    Testing

--- ct/checkmk.sh
+++ ct/checkmk.sh
@@ -64,11 +64,11 @@ msg_ok "Backup Site: ${SITE} to ${SITE}_BACKUP"
 msg_info "Updating ${APP} to v${RELEASE}"
 wget -q --directory-prefix=/opt  https://download.checkmk.com/checkmk/${RELEASE}/check-mk-raw-${RELEASE}_0.bookworm_amd64.deb
 apt-get install -y /opt/check-mk-raw-${RELEASE}_0.bookworm_amd64.deb &>/dev/null 
-echo "${RELEASE}" >"/opt/${APP}_version.txt"
 omd su $SITE &>/dev/null
 omd update --conflict install &>/dev/null
 omd start &>/dev/null
 rm /opt/check-mk-raw-${RELEASE}_0.bookworm_amd64.deb &>/dev/null
+echo "${RELEASE}" >"/opt/${APP}_version.txt"
 exit
 msg_ok "Updating ${APP} to v${RELEASE}"
 
