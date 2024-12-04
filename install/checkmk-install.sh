0c139bdb903b9ca7f9d3e0c80b60c7e89f71c247
Michel Roegl-Brunner <michel.roegl-brunner@htl-braunau.at>
2024-12-04 13:49:00 +0100

    typo

--- install/checkmk-install.sh
+++ install/checkmk-install.sh
@@ -14,11 +14,11 @@ network_check
 update_os
 
 msg_info "Install Dependencies"
-$STD apg-get install -y curl
+$STD apt-get install -y curl
 msg_ok "Install Dependencies"
 
 msg_info "Setup Checkmk"
-RELEASE=curl -s https://checkmk.com/download/archive | grep -oP 'handle="\K[^"]+' | head -n 1
+RELEASE=$(curl -s https://checkmk.com/download/archive | grep -oP 'handle="\K[^"]+' | head -n 1)
 wget -q wget https://download.checkmk.com/checkmk/${RELEASE}/check-mk-raw-${RELEASE}_0.bookworm_amd64.deb
 $STD apt-get install -y ./check-mk-raw-${RELEASE}_0.bookworm_amd64.deb
 echo "${RELEASE}" >"/opt/${APPLICATION}_version.txt"
