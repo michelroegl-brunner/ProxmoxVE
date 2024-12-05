a26ec1cc632eb66068786bb9c9471527ab96f44b
Michel Roegl-Brunner <michel.roegl-brunner@htl-braunau.at>
2024-12-05 15:06:16 +0100

    Changes

--- install/zigbee2mqtt-install.sh
+++ install/zigbee2mqtt-install.sh
@@ -14,31 +14,31 @@ network_check
 update_os
 
 msg_info "Installing Dependencies"
-$STD apt-get install -y curl
-$STD apt-get install -y sudo
-$STD apt-get install -y mc
-$STD apt-get install -y git
-$STD apt-get install -y make
-$STD apt-get install -y g++
-$STD apt-get install -y gcc
-$STD apt-get install -y ca-certificates
-$STD apt-get install -y gnupg
-msg_ok "Installed Dependencies"
+$STD apt-get install -y curl \
+   sudo \
+   mc \
+   git \
+   make \
+   g++ \
+   gcc \
+   ca-certificates \
+   gnupg
+msg_ok "Installing Dependencies"
 
-msg_info "Setting up Node.js Repository"
+msg_info "Setup Node.js Repository"
 mkdir -p /etc/apt/keyrings
 curl -fsSL https://deb.nodesource.com/gpgkey/nodesource-repo.gpg.key | gpg --dearmor -o /etc/apt/keyrings/nodesource.gpg
 echo "deb [signed-by=/etc/apt/keyrings/nodesource.gpg] https://deb.nodesource.com/node_20.x nodistro main" >/etc/apt/sources.list.d/nodesource.list
-msg_ok "Set up Node.js Repository"
+msg_ok "Setup Node.js Repository"
 
-msg_info "Installing Node.js"
+msg_info "Setup Node.js"
 $STD apt-get update
 $STD apt-get install -y nodejs
-msg_ok "Installed Node.js"
+msg_ok "Setup Node.js"
 
-msg_info "Setting up Zigbee2MQTT Repository"
+msg_info "Setup Zigbee2MQTT Repository"
 $STD git clone --depth 1 https://github.com/Koenkk/zigbee2mqtt.git /opt/zigbee2mqtt
-msg_ok "Set up Zigbee2MQTT Repository"
+msg_ok "Setup Zigbee2MQTT Repository"
 
 read -r -p "Switch to Edge/dev branch? (y/N) " prompt
 if [[ $prompt == "y" ]]; then
@@ -47,15 +47,15 @@ else
   DEV="n"
 fi
 
-msg_info "Installing Zigbee2MQTT"
+msg_info "Setup Zigbee2MQTT"
 cd /opt/zigbee2mqtt
 if [[ $DEV == "y" ]]; then
 $STD git fetch origin dev:dev
 $STD git checkout dev
 $STD git pull
 fi
-$STD npm ci
-msg_ok "Installed Zigbee2MQTT"
+$STD pnpm ci --include=dev
+msg_ok "Setup Zigbee2MQTT"
 
 msg_info "Creating Service"
 service_path="/etc/systemd/system/zigbee2mqtt.service"
@@ -73,7 +73,7 @@ User=root
 [Install]
 WantedBy=multi-user.target" >$service_path
 $STD systemctl enable zigbee2mqtt.service
-msg_ok "Created Service"
+msg_ok "Creating Service"
 
 motd_ssh
 customize
