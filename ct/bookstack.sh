44f5909ff43a7de30c4d68d638a2b7e281f4c2ff
Michel Roegl-Brunner <michel.roegl-brunner@htl-braunau.at>
2024-12-10 09:16:57 +0100

    'testin'

--- ct/bookstack.sh
+++ ct/bookstack.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 source <(curl -s https://raw.githubusercontent.com/michelroegl-brunner/ProxmoxVE/refs/heads/dev/misc/build.func)
 # Copyright (c) 2021-2024 community-scripts ORG
-# Author: MickLesk (Canbiz)
-# License: MIT
+#  Author: MickLesk (Canbiz)
+#  License: MIT
 # https://github.com/community-scripts/ProxmoxVE/raw/main/LICENSE
 # Source: https://github.com/BookStackApp/BookStack
 
