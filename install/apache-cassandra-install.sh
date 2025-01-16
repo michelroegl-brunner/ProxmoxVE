b432246b359e7a0be3cad734d94bea4d71f04d84
Michel Roegl-Brunner <73236783+michelroegl-brunner@users.noreply.github.com>
2025-01-16 11:43:29 +0100

    Update apache-cassandra-install.sh

--- install/apache-cassandra-install.sh
+++ install/apache-cassandra-install.sh
@@ -1,8 +1,7 @@
 #!/usr/bin/env bash
 
 # Copyright (c) 2021-2025 tteck
-# Author: tteck (tteckster)
-# License: MIT
+# Author: 
 # https://github.com/community-scripts/ProxmoxVE/raw/main/LICENSE
 
 source /dev/stdin <<< "$FUNCTIONS_FILE_PATH"
