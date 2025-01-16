5e7ee8f4df0a6fc2644060ec87a3e673474bc1f2
Michel Roegl-Brunner <73236783+michelroegl-brunner@users.noreply.github.com>
2025-01-16 10:35:53 +0100

    Update agentdvr.sh

--- ct/agentdvr.sh
+++ ct/agentdvr.sh
@@ -1,6 +1,5 @@
 #!/usr/bin/env bash
-source <(curl -s https://raw.githubusercontent.com/community-scripts/ProxmoxVE/main/misc/build.func)
-# Copyright (c) 2021-2025 tteck
+source <(curl -s
 # Au
 # App Default Values
 APP="AgentDVR"
