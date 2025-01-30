8ad7fdfd333165740b974c0342d1b23791b91368
Michel Roegl-Brunner <michel.roegl-brunner@htl-braunau.at>
2025-01-30 15:15:02 +0100

    Testing

--- install/debian-install.sh
+++ install/debian-install.sh
@@ -14,7 +14,7 @@ network_check
 update_os
 
 msg_info "Installing Dependencies"
-$STD apt-get install -y curl
+$STD apt-get insll -y curl
 $STD apt-get install -y sudo
 $STD apt-get install -y mc
 msg_ok "Installed Dependencies"
