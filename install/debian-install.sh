efba32905a2b838deab5a08131dc10a0dde77369
Michel Roegl-Brunner <michel.roegl-brunner@htl-braunau.at>
2025-02-03 15:24:18 +0100

    Testing

--- install/debian-install.sh
+++ install/debian-install.sh
@@ -6,7 +6,7 @@
 # https://github.com/community-scripts/ProxmoxVE/raw/main/LICENSE
 
 source /dev/stdin <<< "$FUNCTIONS_FILE_PATH"
-source /dev/stdin <<< <(wget -qLO -  https://raw.githubusercontent.com/michelroegl-brunner/ProxmoxVE/refs/heads/develop/misc/api.func)
+#source /dev/stdin <<< "$API_FILE_PATH"
 color
 verb_ip6
 catch_errors
@@ -15,6 +15,7 @@ setting_up_container
 
 msg_info "Installing Dependencies"
 $STD apt-get install -y curl
+post_update_to_api "done" "none"
 $STD apt-get install -y sudo
 $STD apt-get insta -y mc
 msg_ok "Installed Dependencies"
