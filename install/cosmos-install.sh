7db339c7b6fdf15c5b5a141407db04bea9d9f302
Michel Roegl-Brunner <michel.roegl-brunner@htl-braunau.at>
2025-02-04 13:17:55 +0100

    Testing

--- install/cosmos-install.sh
+++ install/cosmos-install.sh
@@ -36,8 +36,8 @@ $STD dpkg -i "mergerfs_${MERGERFS_VERSION}.debian-bullseye_amd64.deb" || $STD ap
 msg_ok "Installed mergerfs"
 
 msg_info "Install Mongo DB"
-curl -fsSL https://www.mongodb.org/static/pgp/server-8.0.asc | gpg --dearmor -o /etc/apt/keyrings/mongodb-server-8.0.gpg
-echo "deb [ signed-by=/usr/share/keyrings/mongodb-server-8.0.gpg ] http://repo.mongodb.org/apt/debian bookworm/mongodb-org/8.0 main" | sudo tee /etc/apt/sources.list.d/mongodb-org-8.0.list
+curl -fsSL https://www.mongodb.org/static/pgp/server-8.0.asc | gpg -o /usr/share/keyrings/mongodb-server-8.0.gpg --dearmor
+echo "deb [ signed-by=/usr/share/keyrings/mongodb-server-8.0.gpg ] http://repo.mongodb.org/apt/debian bookworm/mongodb-org/8.0 main" > /etc/apt/sources.list.d/mongodb-org-8.0.list
 $STD apt-get update
 $STD apt-get install -y mongodb-org
 systemctl enable -q --now mongod
