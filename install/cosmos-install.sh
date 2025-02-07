89081195eb594d1ab38b1e272bdf4b2cf8a87fd7
Rögl-Brunner Michel <Michel.Roegl-brunner@htl-braunau.int>
2025-02-07 08:48:38 +0100

    Testing

--- install/cosmos-install.sh
+++ install/cosmos-install.sh
@@ -36,60 +36,11 @@ $STD dpkg -i "mergerfs_${MERGERFS_VERSION}.debian-bullseye_amd64.deb" || $STD ap
 rm "mergerfs_${MERGERFS_VERSION}.debian-bullseye_amd64.deb"
 msg_ok "Installed mergerfs"
 
-msg_info "Install Mongo DB"
-curl -fsSL https://www.mongodb.org/static/pgp/server-8.0.asc | gpg -o /usr/share/keyrings/mongodb-server-8.0.gpg --dearmor
-echo "deb [ signed-by=/usr/share/keyrings/mongodb-server-8.0.gpg ] http://repo.mongodb.org/apt/debian bookworm/mongodb-org/8.0 main" > /etc/apt/sources.list.d/mongodb-org-8.0.list
-$STD apt-get update
-$STD apt-get install -y mongodb-org
-systemctl enable -q --now mongod
-msg_ok  "Installed Mongo DB"
-
 msg_info "Install Docker"
 curl -fsSL https://get.docker.com -o get-docker.sh
 $STD sh get-docker.sh
 rm get-docker.sh
 msg_ok "Installed Docker"
-
-
-msg_info "Configure MongoDB"
-MONGO_ADMIN_USER="admin"
-MONGO_ADMIN_PWD="$(openssl rand -base64 18 | cut -c1-13)"
-COSMOS_USER="cosmos"
-COSMOS_PWD="$(openssl rand -base64 18 | cut -c1-13)"
-MONGO_CONNECTION_STRING="mongodb://${COSMOS_USER}:${COSMOS_PWD}@localhost:27017/cosmos"
-{
-  echo "Cosmos-Credentials"
-  echo "Mongo Database Admin User: $MONGO_ADMIN_USER"
-  echo "Mongo Database Admin Password: $MONGO_ADMIN_PWD"
-  echo "Cosmos User: $COSMOS_USER"
-	echo "Cosmos Password: $COSMOS_PWD"
-  echo "Mongo Connection String: $MONGO_CONNECTION_STRING"
-} >> ~/cosmos.creds
-
-$STD mongosh <<EOF
-use admin
-db.createUser({
-  user: "$MONGO_ADMIN_USER",
-  pwd: "$MONGO_ADMIN_PWD",
-  roles: [{ role: "root", db: "admin" }]
-})
-
-use cosmos
-db.createUser({
-  user: "$COSMOS_USER",
-  pwd: "$COSMOS_PWD",
-  roles: [
-    { role: "readWrite", db: "cosmos" },
-    { role: "clusterMonitor", db: "admin" }
-  ]
-})
-quit()
-EOF
-sed -i 's/bindIp: 127.0.0.1/bindIp: 0.0.0.0/' /etc/mongod.conf
-sed -i '/security:/d' /etc/mongod.conf
-bash -c 'echo -e "\nsecurity:\n  authorization: enabled" >> /etc/mongod.conf'
-systemctl restart mongod
-msg_ok "Configured MongoDB" 
 
 msg_info "Install Cosmos" 
 mkdir -p /opt/cosmos
@@ -127,9 +78,6 @@ WantedBy=multi-user.target
 EOF
 
 systemctl enable -q --now cosmos.service
-sleep 5
-sed -i "s|\"MongoDB\": *\"[^\"]*\"|\"MongoDB\": \"$MONGO_CONNECTION_STRING\"|" /var/lib/cosmos/cosmos.config.json
-systemctl restart cosmos.service
 msg_info "Created Service"
 
 motd_ssh
