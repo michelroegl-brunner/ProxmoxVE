16ff6b140132f47b2f6e093f10d60123bd23388a
Michel Roegl-Brunner <73236783+michelroegl-brunner@users.noreply.github.com>
2025-02-27 08:20:27 +0100

    Update adventurelog.sh

--- ct/adventurelog.sh
+++ ct/adventurelog.sh
@@ -2,7 +2,7 @@
 source <(curl -s https://raw.githubusercontent.com/community-scripts/ProxmoxVE/main/misc/build.func)
 # Copyright (c) 2021-2025 tteck
 # Author: MickLesk (Caiz)
-# License: MIT | https://github.com/community-scripts/ProxmoxVE/raw/main/LICENSE
+# License: MIT | https://github.com/community-scripts/ProxmoxVE/raw/main/LIC
 # Source: https://adventurelog.app/
 
 APP="AdventureLo
